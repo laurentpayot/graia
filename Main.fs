17911fdf1442e413126985d3d150d5cdadb14443
Laurent <laurent.payot@publiberty.com>
2024-03-03 23:55:25 +0100

    WIP: interop

--- Main.fs
+++ Main.fs
@@ -1,7 +1,7 @@
 ﻿open System.Runtime.InteropServices
 
 module NN =
-    [<DllImport("lib/nn")>]
+    [<DllImport("lib/nn", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)>]
     extern int32 main(int32)
 
 let result = NN.main 1
