53234422913eed26ec88fdc86a8087372dc0e244
Laurent <laurent.payot@publiberty.com>
2024-03-03 23:46:53 +0100

    Futhark lib copied to F#

--- Main.fs
+++ Main.fs
@@ -1,7 +1,7 @@
 ﻿open System.Runtime.InteropServices
 
 module NN =
-    [<DllImport("lib/nn.c", CallingConvention = CallingConvention.Cdecl)>]
+    [<DllImport("lib/nn")>]
     extern int32 main(int32)
 
 let result = NN.main 1
