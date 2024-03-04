2dee3a32b48f34cdbefb9776c73ecdd282a27fcd
Laurent Payot <laurent.payot@publiberty.com>
2024-03-04 11:01:07 +0100

    multiple DllImports

--- Main.fs
+++ Main.fs
@@ -4,7 +4,8 @@ module NN =
     [<DllImport("lib/nn", CallingConvention = CallingConvention.Cdecl)>]
     extern int32 futhark_entry_foo(int32)
 
-    extern int32 futhark_entry_bar()
+    [<DllImport("lib/nn", CallingConvention = CallingConvention.Cdecl)>]
+    extern int32 futhark_entry_bar(int32)
 
 
 printfn "foo: %A" NN.futhark_entry_foo
@@ -12,6 +13,6 @@ printfn "bar: %A" NN.futhark_entry_bar
 
 
 printfn "foo result: %d" <| NN.futhark_entry_foo 100
-printfn "bar result: %d" <| NN.futhark_entry_bar ()
+printfn "bar result: %d" <| NN.futhark_entry_bar 100
 
 printfn "Done"
--- Main.fut
+++ Main.fut
@@ -1,5 +1,5 @@
 entry foo (x: i32): i32 =
   x + 1
 
-entry bar: i32 =
-  42
+entry bar (x: i32): i32 =
+  x + 42
