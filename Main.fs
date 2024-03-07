a5819b0fe0caad578630d4dd2faa5b0f6a5a6888
Laurent Payot <laurent.payot@publiberty.com>
2024-03-07 16:17:07 +0100

    popcount evaluation

--- Main.fs
+++ Main.fs
@@ -1,1 +1,16 @@
 ﻿printfn "🌄 Graia v0.0.1"
+
+
+open System.Runtime.Intrinsics.X86
+
+if not (Popcnt.IsSupported) then
+    printfn "Popcnt is not supported"
+    exit 1
+
+let n = 7UL
+
+
+let c = Popcnt.X64.PopCount n
+
+
+printfn "%d" c
