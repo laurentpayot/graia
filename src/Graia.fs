61a4e955acc80904955578e490ce6d52cd44462d
Laurent Payot <laurent.payot@publiberty.com>
2024-03-13 11:35:24 +0100

    bit array evaluation

--- src/Graia.fs
+++ src/Graia.fs
@@ -2,12 +2,25 @@
 
 printfn "🌄 Graia v0.0.1"
 
-open System.Runtime.Intrinsics.X86
 open System.Runtime.Intrinsics
-open System.Numerics
-
-
-printfn "Popcnt.X64: %b" Popcnt.X64.IsSupported
-printfn "Vector: %b" Vector.IsHardwareAccelerated
+printfn "Vector128: %b" Vector128.IsHardwareAccelerated
 printfn "Vector256: %b" Vector256.IsHardwareAccelerated
 printfn "Vector512: %b" Vector512.IsHardwareAccelerated
+
+open System.Collections
+
+let a: BitArray = BitArray(3)
+let b: BitArray = BitArray(3)
+
+a.Set(1, true)
+b.Set(2, true)
+// let b = Seq.iter (fun x -> printfn $"%A{x}") a
+
+// for x, y in Seq.zip a.GetEnumerator() b.GetEnumerator() do
+//     printfn $"%A{x}"
+
+for x in a do
+    printfn $"%A{x}"
+
+printfn $"%A{a}"
+printfn $"%A{b}"
